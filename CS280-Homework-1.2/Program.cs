9f842fdfe776df85d1b8c4b849d61cbd0062465c
halicacabum <halicacabum@gmail.com>
2017-03-05 03:46:22 +0800

    加入Pokemon類別

--- CS280-Homework-1.2/Program.cs
+++ CS280-Homework-1.2/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace CS280_Homework_1._2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Number = 4f;
+            pokemon.Name = "Charmander";
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 18.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+            Console.WriteLine("編號:{0},名稱:{1},身高:{2}公尺,體重{3}磅,類別:{4},能力:{5},性別:{6}", pokemon.Number, pokemon.Name, pokemon.Height, pokemon.Weight, pokemon.Category, pokemon.Abilities, pokemon.Gender);
         }
     }
 }
